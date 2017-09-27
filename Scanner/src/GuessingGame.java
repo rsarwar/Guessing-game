f739c336388a17584acc46e88de522ccc1d35efc
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-09-27 12:44:22 -0400

    Hello

--- Scanner/src/GuessingGame.java
+++ Scanner/src/GuessingGame.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.lang.Math;
 /*Ramisha Sarwar
- * fdsghdrtgfsdfsefhdthr
+ * fdsghdrtgfsdfsefhdthrtetert
  */
 public class GuessingGame {
 	public static void main (String[] args)
